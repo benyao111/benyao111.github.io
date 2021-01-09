c5c7ae8aa6fd5342362e401e1463d6923ab9f1af
Benjamin Yao <benyao111@gmail.com>
2021-01-08 22:17:14 -0800

    new updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Mainpage from './components/mainpage/mainpage'
 import './App.css';
+import {Route, Switch} from 'react-router-dom'
+import AboutMe from '../src/containers/AboutMe/AboutMe'
+import Projects from '../src/containers/Projects/Projects'
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+        <Route path = '/' exact component = {Mainpage}/>
+        <Route path = '/aboutme' component = {AboutMe}/>
+        <Route path = '/projects' component = {Projects}/>
+        </Switch>
+      </div>
+      );
+    }
 }
-
 export default App;
