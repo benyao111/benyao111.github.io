ec8b26b65f0260c352fec400e72d553dd554e8ae
Ben Yao <benyao111@gmail.com>
2025-03-30 03:37:37 -0400

    new changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import cat from './cat.jpg'
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <div className="top">Donate</div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className = "gridd">
+          <img src={cat} className = "profile-img" alt="cat" />
+          <div>
+            <p>
+              Ben Yao
+            </p>
+            <div>Auditor at Comcast</div>
+          </div>
+        </div>
+        
       </header>
+      <div>
+        RESUME
+      </div>
+      <div>Projects</div>
+      <div>
+        Donate (could go up top as a header top right or something)
+      </div>
     </div>
   );
 }
